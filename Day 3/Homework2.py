e510d283390343d04d83d1d6f961b8997e904bf4
Lashaa2 <wamalaidzelasha94@gmail.com>
2023-07-14 20:04:10 +0400

    first commit

---
+++ Day 3/Homework2.py
@@ -0,0 +1,54 @@
+44Name - 3Lasha
+21saxli %% b44atmanhouse
+2pr_int + sax4li
+2print = "Lasha"
+/ = 4rt
+2m4ood = 2g2
+333prtint=(2mood)
+4het4ight = 15"8
+3heigh- + prtint
+44car + Corvrr45ett 
+2lowprice = \
+2highprice = ]]
+print(end of program")
+       2print =/=('saxeli'")
+ 2saxeli="chevrolet"
+
+
+
+22print = (__name__ -- print")
+2namecar <<- bmw
+2umodebmw<< ((wamlidze))
+
+33speed ++ ('999 """""') 
+3@carname=(BMw#)
+
+Name ="Lasha"
+Saxli = "Batmanhouse"
+print(Saxli)
+print(Name)
+print(4)
+mood="sad"
+print(mood)
+Heigh= 158
+print(Heigh)
+car="corvett"
+Lowprice=10000
+Highprice=20000
+print(Lowprice + Highprice)
+Saxeli="Chevrolet"
+print(Saxeli)
+
+Namecar="BMW"
+print(Namecar)
+modelbmw="series three"
+speed=220
+maxspeed=300
+speedarein="kmh"
+print(speed)
+print(maxspeed)
+print(speed * maxspeed)
+print(maxspeed + speed)
+print(Heigh * Lowprice)
+print(Highprice * Lowprice)
+print(Heigh * Lowprice)
