ef2f852a17ca87f836ecd10530e53b988a7cb1a5
Lashaa2 <wamalaidzelasha94@gmail.com>
2023-07-18 10:23:39 +0400

    first commit

---
+++ Homework 4/HomeWork 4.py
@@ -0,0 +1,8 @@
+user_name=input("whats your name: ")
+Surname=input("whats your surname: ")
+age=input("how old are you: ")
+time_travel=input("How many years do you want to travel: ")
+after_time_travel=input("how old you will be after time travel: ")
+print("Her name is " + user_name + ". Her surname is " + Surname + ". Her age is " + str(age) +
+      ". She wants to travel. Time to travel: " + str(time_travel) + ". Her age after travel: " + str(after_time_travel) + ".")
+
