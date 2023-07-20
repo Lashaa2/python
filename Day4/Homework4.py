67053db0681165967d6f286eca0fc300e6540d9a
Lashaa2 <wamalaidzelasha94@gmail.com>
2023-07-20 23:41:27 +0400

    first commit

---
+++ Day4/Homework4.py
@@ -0,0 +1,8 @@
+name=input("ra gqvia: ")
+surname=input("sheni gvari: ")
+age=input("ramdeni wlis xar: ")
+fav_food=input("sayvareli skvebi: ")
+birth_year=input("dabadebis weli: ")
+pc=input("komp gaq: ")
+phone=input("telefoni gaq: ")
+print(name + " " + surname ,"misi asaki" , str(age), " " , "sayvareli saymeli" , fav_food , str(birth_year) ,"kompiuteri aqvs tu ara" , pc ," tele aqvs tu ara "+ phone)
