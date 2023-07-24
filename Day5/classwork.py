a053a0b756dc7cadea44d9f880e9ee314acdd062
Lashaa2 <wamalaidzelasha94@gmail.com>
2023-07-24 23:46:53 +0400

    first commit

---
+++ Day5/classwork.py
@@ -0,0 +1,27 @@
+#
+
+
+
+# age = 15 # INTEGER
+# saxeli = '20' # STRING
+# yiyliyo = 6.5 # FLOAT
+# arisBangeri = True # BOOL - BOOLEAN
+
+# print(arisBangeri)
+
+# 0 1 - orobiti - binary
+# 0 - False
+# 1 - True
+
+
+print(25 != 25)
+
+
+# შედარების ოპერატორები
+# =  მინიჭების ოპერატორი
+# ==  უდრის
+# !=  არ უდრის
+# >  მეტობა
+# <  ნაკლებობა
+# >=
+# <=
---
+++ Day5/homework.py
@@ -0,0 +1,8 @@
+your_age=input("ramdeni wlis xar")
+print(18< True)
+
+
+your_age=input("sheni asaki; ")
+your_dad_age=input("mamashenis asaki; ")
+mati_asaki=(int(your_age)+int(your_dad_age))
+print(50< True)
