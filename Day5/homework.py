2e9544b5800c74e0c81775991a445d5b46af021b
Lashaa2 <wamalaidzelasha94@gmail.com>
2023-07-25 13:22:46 +0400

    first commit

--- Day5/homework.py
+++ Day5/homework.py
@@ -1,8 +1,8 @@
 your_age=input("ramdeni wlis xar")
-print(18< True)
+print(18< int(your_age))
 
 
 your_age=input("sheni asaki; ")
 your_dad_age=input("mamashenis asaki; ")
 mati_asaki=(int(your_age)+int(your_dad_age))
-print(50< True)
+print(60< int(mati_asaki))
