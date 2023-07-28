1333b8a6f55c9e006cc2f7b29c22f15aca1a845e
Lashaa2 <wamalaidzelasha94@gmail.com>
2023-07-28 18:45:01 +0400

    first commit

---
+++ Day7/classwork.py
@@ -0,0 +1,13 @@
+# name = "vano" #str
+# age = 16 #int
+# height = 186.5 #float
+# knows_programming = True #boolean
+# # devide /      
+# # multiply *
+# # xarisxi **
+# # mimateba + 
+# # gamokleba - 
+# # ramdenjer motavsdeba = //
+# # udris tu ara - ==
+# # nashti %
+# print(age + int(height))
---
+++ Day7/homework.py
@@ -0,0 +1,12 @@
+price=int(300)
+low_price=int(200)
+print(price > low_price or low_price > price)
+#second code
+ 
+team_goal=int(2001)
+league_goal=int(4890)
+
+both_goal=team_goal+league_goal
+all_goal=int(50987)
+print(team_goal > league_goal and both_goal > all_goal)
+print(both_goal < all_goal and league_goal>team_goal)
