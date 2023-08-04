a4c8416066a4b97b53c7982494a05c396f10d4d1
Lashaa2 <wamalaidzelasha94@gmail.com>
2023-08-04 12:37:02 +0400

    first commit

---
+++ Day8/classwork.py
@@ -0,0 +1,11 @@
+#for i in range(3):
+    #print("Hello")
+#for i in range(10):
+    #print(i)
+    #print("nika")
+#for i in range(10):
+   # print("nika" + str(i))
+#for i in range(1 ,20 , 1 ):
+#for i in range(1 ,20 , 2):
+    print(i , str("lasha"))
+    print(i , str("wamalaidze"))
