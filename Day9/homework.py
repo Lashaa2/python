9e39abaeafa1dfc564e1e35baec07b4276702350
Lashaa2 <wamalaidzelasha94@gmail.com>
2023-08-08 13:40:27 +0400

    second commit

--- Day9/homework.py
+++ Day9/homework.py
@@ -2,10 +2,18 @@
 #მომხმარებელს უნდა შემოვატანინოთ მისი ასაკი და რამდენი წლისაცაა იმდენჯერ 
 #დაპრინტოს გილოცავ შენ გახდი 0,1,2 წლის
 age = int(input("Your Age: "))
-while age > 0:
-    print("glocav shen gaxdi" , age)
-    age= age-1
+num = 0
+while age > num:
+    print("glocav shen gaxdi", num )
+    num= num +1
+    while num == 8:
+        print("gilocav shen gaxdi ", num ," wlis da shemoxvedi GOAshi" )
+        num= num + 1
+
+
 
 #დავალება N2 დაპრინტეთ 1 დან 100მდე ყველა კენტი რიცხვი
-for i in range(1,102,2):
-    print(int(i))
+number= 1
+while number < 100:
+    print(number)    
+    number= number + 2
--- Day9/lessonwork.py
+++ Day9/lessonwork.py
@@ -3,3 +3,6 @@
 #    print("sell ticket")
 #seats= seats - 2
 #shemataninet misi asaki d ramdeni wlisac aris imdenjer daiprintos gilocav shen gaxdi 0,1,2,3...
+
+
+
