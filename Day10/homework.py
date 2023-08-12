6213fb065ca6d66bd44350e8cd4ef41e26d042af
Lashaa2 <wamalaidzelasha94@gmail.com>
2023-08-12 15:44:21 +0400

    first commit

---
+++ Day10/homework.py
@@ -0,0 +1,40 @@
+#davaleba N1
+number=0
+while number < 125:
+  if number == 1:
+    print(number)
+  else:
+    print("this number", number +2 , "luwia")
+    if number == 1:
+      print(number)
+    else:
+      print("es rivxvi", number + 1 ,"kentia")
+  number=number +2
+#davaleba N2
+asaki=int(input("ramdeni wlis xar :"))
+mamashenis_asaki=int(input("mamashenis asaki :"))
+if mamashenis_asaki == asaki * 2:
+  print("Bingoo")
+else:
+  print("Mouse")
+
+
+
+
+##davaleba N3
+age=int(input("sheni asakki :"))
+if age >= 65:
+  print("tqven xart moxucebuli")
+else:
+  print("tqven pensia jer ar gekutvnit")
+
+sqesi=input("romel sqess warmoadgent")
+if sqesi == "qalbatoni" or sqesi == "mamakaci" or sqesi == "qali" or sqesi == "kaci":
+  print("tqven  gekutvnit pensia ")
+else:
+  print("ra arseba xar aba an ra sqesisss dd")
+
+
+
+
+
