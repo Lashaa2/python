5abdcd5b5a5c79725c7765c1cb28ac283be1f184
Lashaa2 <wamalaidzelasha94@gmail.com>
2023-08-22 23:19:06 +0400

    second commit

--- Day13/homework.py
+++ Day13/homework.py
@@ -1,8 +1,9 @@
-saxeli_gvari=["L" , "a" , "s" , "h" , "a" , "w" ,"a","m","a","l","a","i","d","z","e"]
+saxeli_gvari=["l" , "a" , "s" , "h" , "a" , "w" ,"a","m","a","l","a","i","d","z","e"]
 num=0
 i=0
-while (i < len(saxeli_gvari)):
+while(i < len(saxeli_gvari)):
     if saxeli_gvari[i] != "a" and  saxeli_gvari[i] != "a"  and saxeli_gvari[i] != "e" and  saxeli_gvari[i] != "i" and saxeli_gvari[i] != "o"  and saxeli_gvari[i] != "u" :
-        num=num+1
-        i=i+1
-        print(num)
+        num=num + 1
+    i=i+1
+    
+    print(num)
