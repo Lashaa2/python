6fce3a5a1e73a2060aa6820a93e0b068ecd50c70
Lashaa2 <wamalaidzelasha94@gmail.com>
2023-08-29 14:36:54 +0400

    first commit

---
+++ Day15/classwork.py
@@ -0,0 +1,4 @@
+#
+#save=["m","o","n","e","y"]
+#print(save[ :4 ])
+#print(save[1:])
---
+++ Day15/homework.py
@@ -0,0 +1,15 @@
+value1="saqartvelo"
+print(value1[2:])
+print(value1[6:])
+#ანნ
+print(value1[-8:])
+print(value1[-4:])
+
+
+#N2
+value2='dedamiwa'
+print(value2[:4])
+print(value2[4:])
+#ანნ
+print(value2[-4:])
+print(value2[:-4])
