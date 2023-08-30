000fe03f54f6c974b0f3e515c4fb9725686d8dc1
Lashaa2 <wamalaidzelasha94@gmail.com>
2023-08-30 10:50:28 +0400

    first commit

--- Day15/homework.py
+++ Day15/homework.py
@@ -13,3 +13,13 @@ print(value2[4:])
 #ანნ
 print(value2[-4:])
 print(value2[:-4])
+
+#n3
+value3=["sa","qa","rt","velo","deda","miwa" ,"deda","ena"]
+print(value3[:4])
+print(value3[1:4])
+print(value3[3])
+print(value3[4])
+print(value3[4:6])
+print(value3[6])
+print(value3[6:])
